1ae5a5951a2398823ab0059f8ef02200ea06a683
unknown <cynthiaho711@gmail.com>
2016-06-23 11:45:30 +0200

    update structure for application to work

--- FlaskApp/app/run.py
+++ FlaskApp/app/run.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, send_from_directory, request
-import http.client, urllib.request, urllib.parse, urllib.error, base64, requests
 import json
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
     print("in index")
     return render_template('index.html', test_context="this is a string from the OTHER side!")
+
+@app.route("/search.html")
+def search():
+    print("in search")
+    return render_template('search.html', test_context="this is a string from the OTHER side!")
 
 @app.route('/js/<path:path>')
 def send_js(path):
@@ -19,11 +24,15 @@ def send_css(path):
     return send_from_directory('css', path)
 
 
-@app.route('/images/<path:path>')
+@app.route('/img/<path:path>')
 def send_img(path):
-    return send_from_directory('images', path)
+    return send_from_directory('img', path)
 
 
 @app.route('/fonts/<path:path>')
 def send_font(path):
     return send_from_directory('fonts', path)
+
+
+if __name__ == '__main__':
+	app.run()
